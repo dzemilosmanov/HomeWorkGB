b2b67d6a1e1d221acb97f6d45b647b4873118966
dzemilosm <dzemilosm@gmail.com>
2018-10-24 10:48:24 +0300

    ДЗ урок 1.

---
+++ Lesson1HW/src/test/java/ru/osmanov/hw/AppTest.java
@@ -0,0 +1,20 @@
+package ru.osmanov.hw;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
