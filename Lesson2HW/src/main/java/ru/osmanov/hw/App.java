996d90653a19a82749149e1f617faf7a36d57451
dzemilosmanov <44261535+dzemilosmanov@users.noreply.github.com>
2018-10-24 15:11:43 +0300

    Update App.java

--- Lesson2HW/src/main/java/ru/osmanov/hw/App.java
+++ Lesson2HW/src/main/java/ru/osmanov/hw/App.java
@@ -37,8 +37,7 @@ public class App {
     public static void changeArray() {
 
         int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        for (int i : arr) {
-        }
+     
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] < 6) arr[i] *= 2;
         }
